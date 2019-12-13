43256da50d72f9c493b93598facea5e75fdead44
Daniel Aguilar <dacmail@gmail.com>
2019-12-13 10:50:19 +0100

    TipiHeader component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import TipiResults from './Results/Results.vue'
 import TipiMessages from './Messages/Messages.vue'
 import TipiCsvDownload from './CSVDownload/CSVDownload.vue'
+import TipiHeader from './Header/Header.vue'
 
 export {
   TipiResults,
   TipiMessages,
-  TipiCsvDownload
+  TipiCsvDownload,
+  TipiHeader,
 }
