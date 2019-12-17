4c19a999638fc94d93f9efbfcbbfe4c1bd24e72a
Daniel Aguilar <dacmail@gmail.com>
2019-12-17 16:22:13 +0100

    tipi-results with option between tiny and extended layout

--- src/components/Results/Results.vue
+++ src/components/Results/Results.vue
@@ -2,15 +2,15 @@
   <div>
     <div v-if="this.loadingResults" class="text-center"><h2>Cargando resultados...</h2></div>
     <table
-      v-if="this.initiatives.length && !this.loadingResults"
+      v-if="this.initiatives && this.initiatives.length && !this.loadingResults"
       id="reactive-table-1"
       class="table table-striped table-hover reactive-table">
       <thead>
         <tr>
           <th class="col-md-6" fieldid="titulo">Titulo</th>
-          <th class="col-md-2" fieldid="autor">Autor</th>
-          <th class="col-md-1" fieldid="grupo">Grupo</th>
-          <th class="col-md-2" fieldid="temas">Temas</th>
+          <th v-if="extendedLayout" class="col-md-2" fieldid="autor">Autor</th>
+          <th v-if="extendedLayout" class="col-md-1" fieldid="grupo">Grupo</th>
+          <th v-if="extendedLayout" class="col-md-2" fieldid="temas">Temas</th>
           <th class="sortable col-md-1" fieldid="actualizacion">
             Fecha <i class="fa fa-sort-desc"></i>
           </th>
@@ -21,9 +21,9 @@
           <td class="titulo">
             <router-link :to="{path: '/initiatives/' + initiative.id}">{{initiative.title}}</router-link>
           </td>
-          <td class="autor_diputado" v-html="getDeputies(initiative)"></td>
-          <td class="autor_grupo" v-html="getAuthors(initiative)"></td>
-          <td v-html="getTopics(initiative)"></td>
+          <td v-if="extendedLayout" class="autor_diputado" v-html="getDeputies(initiative)"></td>
+          <td v-if="extendedLayout"class="autor_grupo" v-html="getAuthors(initiative)"></td>
+          <td v-if="extendedLayout" v-html="getTopics(initiative)"></td>
           <td class="actualizacion"><span :sort="initiative.updated">{{ moment(initiative.updated).format('DD/MM/Y') }}</span></td>
         </tr>
       </tbody>
@@ -46,6 +46,7 @@ export default {
       default: function() { return [] },
     },
     queryMeta: Object,
+    layout: String,
   },
   data: function() {
     return{
@@ -60,6 +61,9 @@ export default {
       let nextResult = (this.queryMeta.page * this.queryMeta.per_page) + 1;
       let lastResult = nextResult + this.queryMeta.per_page -1;
       return `(${nextResult}-${lastResult})`;
+    },
+    extendedLayout: function() {
+      return this.layout!='tiny';
     }
   },
   methods: {
