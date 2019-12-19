ea3c37954a8b4133aa8d39e3fda04e95b01bf610
Daniel Aguilar <dacmail@gmail.com>
2019-12-19 11:19:16 +0100

    InitiativeMeta component shows initiative status and link to source

---
+++ src/components/InitiativeMeta/InitiativeMeta.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div :class="`text-center state well color-${getColorByStatus(initiative.status)}`">
+      <strong>{{ initiative.status }}</strong>
+    </div>
+    <p class="congress-link text-center">
+      <a :href="initiative.url" target="_blank" :title="`Ver ${initiative.title} en su fuente original`">
+        <i class="fa fa-institution"></i><span>{{ linkText }}</span>
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TipiInitiativeMeta',
+  props: {
+    initiative: Object,
+    linkText: String,
+    colors: {
+      type: Object,
+      default: function() {
+        return {
+          'green': ['Aprobada', 'Respondida', 'Celebrada', 'Convertida en otra', 'Acumulada en otra'],
+          'black': ['En tramitación', 'Desconocida'],
+          'red': ['No admitida a trámite', 'No debatida', 'Rechazada', 'Retirada', 'Derogada', 'No celebrada'],
+        };
+      },
+    },
+  },
+  methods: {
+    getColorByStatus: function(status) {
+      for (let color in this.colors) {
+        if (this.colors[color].indexOf(status) != -1) return color;
+      }
+      return 'black';
+    },
+  }
+}
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ import TipiNavbar from './Navbar/Navbar.vue'
 import TipiDeputy from './Deputy/Deputy.vue'
 import TipiText from './Text/Text.vue'
 import TipiTopics from './Topics/Topics.vue'
+import TipiInitiativeMeta from './InitiativeMeta/InitiativeMeta.vue'
 
 export {
   TipiResults,
@@ -16,4 +17,5 @@ export {
   TipiDeputy,
   TipiText,
   TipiTopics,
+  TipiInitiativeMeta,
 }
