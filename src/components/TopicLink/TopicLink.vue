64a573314aaa7bc54e34bb75adc6f1edf7320329
Daniel Aguilar <dacmail@gmail.com>
2020-02-13 17:10:02 +0100

    TopicLink and header fixes

--- src/components/TopicLink/TopicLink.vue
+++ src/components/TopicLink/TopicLink.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <router-link :to="{path: '/topics/' + topic.id}">
-      <img :src="`/img/topics/${topic.icon}`" :alt="`Imagen de ${topic.name}`">
-      <h4>{{ topic.name }}</h4>
-    </router-link>
+    <div class="c-topic-link">
+      <router-link :to="{path: '/topics/' + topic.id}" class="c-topic-link__image-link" :style="`background-color: ${color}`">
+        <img :src="topicImage" :alt="`Imagen de ${topic.name}`" class="c-topic-link__image">
+      </router-link>
+      <h4 class="c-topic-link__name">
+        <router-link :to="{path: '/topics/' + topic.id}">
+          {{ topic.name }}
+        </router-link>
+      </h4>
+    </div>
   </div>
 </template>
 
@@ -11,7 +17,17 @@
 export default {
   name: 'TipiTopicLink',
   props: {
+    color: {
+      type: String,
+      default: '#000',
+    },
+    image: String,
     topic: Object,
+  },
+  computed: {
+    topicImage: function() {
+      return `/img/topics/${this.image}` || `/img/topics/${this.topic.icon}`;
+    },
   },
 };
 </script>
