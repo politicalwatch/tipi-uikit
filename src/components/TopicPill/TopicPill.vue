68c21c9f53338b6d0d02ca18f495088fb52c9a06
Daniel Aguilar <dacmail@gmail.com>
2020-02-26 16:47:34 +0100

    TopicPill now accepts links

--- src/components/TopicPill/TopicPill.vue
+++ src/components/TopicPill/TopicPill.vue
@@ -10,16 +10,25 @@ export default {
   props: {
     initiative: Object,
     topicsStyles: Object,
+    withLinks: Boolean,
   },
   methods: {
     getTopics: function(initiative) {
       if (this.topicsStyles && initiative.hasOwnProperty('topics')) {
-        return initiative.topics.slice().sort(Utils.naturalSort).map(element => {
-          return `
-            <div class="c-topics__topic" style="background-color:${this.topicsStyles[element].color}">
-              ${this.topicsStyles[element].shortname}
-            </div>
-          `;
+        return initiative.topics.slice().sort(Utils.naturalSort).map((element, i)=> {
+          if (this.withLinks) {
+            return `
+              <a href="#topic-${i}" class="c-topics__topic" style="background-color:${this.topicsStyles[element].color}">
+                ${this.topicsStyles[element].shortname}
+              </a>
+            `;
+          } else {
+            return `
+              <div class="c-topics__topic" style="background-color:${this.topicsStyles[element].color}">
+                ${this.topicsStyles[element].shortname}
+              </div>
+            `;
+          }
         }).join('');
       }
       return initiative.hasOwnProperty('topics') ?
