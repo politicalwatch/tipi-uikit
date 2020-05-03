7d849f5594df9ef9d15e337b9968b3fca82d3c3a
Santi Espinosa <saigesp@gmail.com>
2020-05-03 14:14:05 +0200

    add TipiChart (simple charts made only with vue)

--- src/components/index.js
+++ src/components/index.js
@@ -16,6 +16,7 @@ import TipiIcon from './Icon/Icon.vue';
 import TipiInitiativeCard from './InitiativeCard/InitiativeCard.vue';
 import TipiTopicPill from './TopicPill/TopicPill.vue';
 import TipiLoader from './Loader/Loader.vue';
+import TipiBarchart from './TipiCharts/TipiBarchart.vue';
 import D3Sunburst from './Charts/Sunburst/d3.sunburst.vue';
 import D3WordsCloud from './Charts/WordsCloud/d3.wordscloud.vue';
 import D3BarChart from './Charts/Barchart/d3.barchart.vue';
@@ -41,6 +42,7 @@ export {
   TipiInitiativeCard,
   TipiTopicPill,
   TipiLoader,
+  TipiBarchart,
   D3Sunburst,
   D3WordsCloud,
   D3BarChart,
