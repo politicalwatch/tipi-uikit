c804941a2880137c3c91dea3714e7e8bbbebf024
David Hernández <davidhernandezruiz@gmail.com>
2020-10-26 13:32:23 +0100

    Adds small transparency to compared data (#15)
    * Adds small transparency to compared data to make it easier to differenciate from the original data.
    
    * Makes sure is the compared bar the one that is changed.

--- src/components/Charts/Barchart/d3.barchart.js
+++ src/components/Charts/Barchart/d3.barchart.js
@@ -188,11 +188,14 @@ class d3barchart extends d3chart {
       .data(this.data, d => d[this.cfg.key]);
   }
 
+  hasComparissionData() {
+    return this.data.some(item => item["Texto comparado"] > 0)
+  }
+
   /**
    * Add new chart's elements
    */
   enterElements() {
-
     const newbars = this.itemg
       .enter().append('g')
       .attr('class', 'chart__bar-group chart__bar-group--barchart')
@@ -202,7 +205,7 @@ class d3barchart extends d3chart {
         }
         return `translate(0,${this.xScale(d[this.cfg.key])})`;
       });
-      
+
     const rects = newbars.selectAll('.chart__bar')
       .data(d => this.cfg.values.map(v => {
         const dat = { ...d };
@@ -267,6 +270,7 @@ class d3barchart extends d3chart {
       .selectAll('.chart__bar')
       .transition(this.transition)
       .attr('fill', (d, i) => this.colorElement(d, this.cfg.values[i % this.cfg.values.length]))
+      .attr('opacity', (d, i) => i % 2 == 1 && this.hasComparissionData() ? 0.6 : 1)
       .attr('x', (d, i) => {
         return this.cfg.orientation !== 'horizontal'
           ? this.xScaleInn(this.cfg.values[i % this.cfg.values.length])
