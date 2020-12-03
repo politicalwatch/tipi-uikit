6bf3c6f0b44e7c62c13f6a28ed17480484b5dcc2
David Hernandez <davidhernandezruiz@gmail.com>
2020-12-03 10:12:21 +0100

    Adds capability to include external links on the navbar and adds missing class in the icons.

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -29,7 +29,8 @@
           v-for="link in links"
           :key="link.route"
           v-show="link.condition">
-            <router-link :to="{name: link.route }" class="c-menu__link">{{ link.name }}<icon :icon="link.icon" v-if="link.name" /></router-link>
+            <a v-if="link.external" :href="link.route" target="_blank">{{ link.name }}<icon :icon="link.icon" class="c-menu__icon" v-if="link.icon" /></a>
+            <router-link v-else :to="{name: link.route }" class="c-menu__link">{{ link.name }}<icon :icon="link.icon" class="c-menu__icon" v-if="link.icon" /></router-link>
           </li>
         </ul>
       </nav>
