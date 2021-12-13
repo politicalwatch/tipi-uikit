0e7abd3dff3cd25f7f1b8ca61f6d860f6a0939f6
David Hernández <davidhernandezruiz@gmail.com>
2021-12-13 21:47:30 +0100

    Adds new mixing to allow changing filling of SVGs and uses it to override the filling of the congress link so it is not broken on the TIPI site.

--- src/components/CongressLink/CongressLink.vue
+++ src/components/CongressLink/CongressLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="url" class="c-congress-link u-color-primary" target="_blank" title="Ver en el Congreso.es"><tipi-icon icon="open-blank"></tipi-icon>Ver en el Congreso.es</a>
+  <a :href="url" class="c-congress-link u-color-primary" target="_blank" title="Ver en el Congreso.es"><tipi-icon class="u-filling-primary" icon="open-blank"></tipi-icon>Ver en el Congreso.es</a>
 </template>
 
 <script>
