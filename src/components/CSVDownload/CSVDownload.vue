50f3d1a3edafe20f195e4720629334725e4e4186
Alex Ahumada <alex.ahumada@politicalwatch.es>
2023-10-09 18:26:48 +0200

    Fixes issue with csv download

--- src/components/CSVDownload/CSVDownload.vue
+++ src/components/CSVDownload/CSVDownload.vue
@@ -3,51 +3,95 @@
     <a
       v-if="!csvItems.length"
       :class="{ disabled: !canDownloadCSV }"
-      :title="!canDownloadCSV ? 'Demasiados resultados para poder descargar. Afina la búsqueda' : 'Descarga CSV con todos los resultados'"
+      :title="
+        !canDownloadCSV
+          ? 'Demasiados resultados para poder descargar. Afina la búsqueda'
+          : 'Descarga CSV con todos los resultados'
+      "
       @click.prevent="loadCSVItems"
-      class="c-button c-button--compact c-button--secondary c-button--icon-right" href="#">
-        {{ label }}
-        <span class="c-icon c-icon--type-download">
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="none" viewBox="0 0 12 16">
-            <path fill="#2D4252" d="M12 5.647H8.571V0H3.43v5.647H0l6 6.588 6-6.588zm-12 8.47V16h12v-1.882H0z"></path>
-          </svg>
-        </span>
+      class="c-button c-button--compact c-button--secondary c-button--icon-right"
+      href="#"
+    >
+      {{ label }}
+      <span class="c-icon c-icon--type-download">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="16"
+          fill="none"
+          viewBox="0 0 12 16"
+        >
+          <path
+            fill="#2D4252"
+            d="M12 5.647H8.571V0H3.43v5.647H0l6 6.588 6-6.588zm-12 8.47V16h12v-1.882H0z"
+          ></path>
+        </svg>
+      </span>
     </a>
-    <vue-csv-downloader
+    <json-excel
       v-else
       :data="csvItems"
       :fields="csvFields"
-      :downloadName="getNameFromCSV()"
+      :name="getNameFromCSV()"
       id="downloadCSV"
-      class="c-button c-button--icon-right" :class="buttonClass">
-        {{ label }}
-        <span class="c-icon c-icon--type-download">
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" fill="none" viewBox="0 0 12 16">
-            <path fill="#2D4252" d="M12 5.647H8.571V0H3.43v5.647H0l6 6.588 6-6.588zm-12 8.47V16h12v-1.882H0z"></path>
-          </svg>
-        </span>
-    </vue-csv-downloader>
+      type="csv"
+      class="c-button c-button--icon-right"
+      :class="buttonClass"
+    >
+      {{ label }}
+      <span class="c-icon c-icon--type-download">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="16"
+          fill="none"
+          viewBox="0 0 12 16"
+        >
+          <path
+            fill="#2D4252"
+            d="M12 5.647H8.571V0H3.43v5.647H0l6 6.588 6-6.588zm-12 8.47V16h12v-1.882H0z"
+          ></path>
+        </svg>
+      </span>
+    </json-excel>
   </span>
 </template>
 
 <script>
-import VueCsvDownloader from 'vue-csv-downloader';
+import JsonExcel from 'vue-json-excel3';
 
 export default {
   name: 'TipiCsvDownload',
   components: {
-    VueCsvDownloader,
+    JsonExcel,
   },
   props: {
     initiatives: {
       type: Array,
-      default: function() { return []; },
+      default: function () {
+        return [];
+      },
     },
     csvItems: Array,
     canDownloadCSV: Boolean,
     csvFields: {
       type: Array,
-      default: function() { return ['title', 'reference', 'initiative_type_alt', 'authors', 'deputies', 'tagged.0.topics', 'tagged.0.tags', 'place', 'status',  'created','updated', 'url']; },
+      default: function () {
+        return [
+          'title',
+          'reference',
+          'initiative_type_alt',
+          'authors',
+          'deputies',
+          'tagged.0.topics',
+          'tagged.0.tags',
+          'place',
+          'status',
+          'created',
+          'updated',
+          'url',
+        ];
+      },
     },
     label: {
       type: String,
@@ -59,12 +103,12 @@ export default {
     },
   },
   methods: {
-    loadCSVItems: function(event) {
+    loadCSVItems: function (event) {
       this.$emit('loadCSVItems', event);
     },
-    getNameFromCSV: function() {
+    getNameFromCSV: function () {
       let d = new Date();
-      return "export-" + d.toISOString() + ".csv";
+      return 'export-' + d.toISOString() + '.csv';
     },
   },
 };
