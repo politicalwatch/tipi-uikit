92ee4634ccc37945fe4915da43ead0836cb0dc08
Alex Ahumada <alex.ahumada@politicalwatch.es>
2024-03-19 14:13:27 +0100

    Updates Storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,6 @@ import '../src/styles/main.scss';
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: '^on.*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
