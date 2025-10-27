55c4bdd9a6bf0a83fd32382c4e604a526b7a0a44
Alex Ahumada <alex.ahumada@politicalwatch.es>
2025-10-27 09:21:27 +0100

    feat: upgrade storybook to v9, vite to v7 and vitest to v4

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,8 @@ export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@chromatic-com/storybook',
+    '@storybook/addon-docs'
   ],
   framework: {
     name: '@storybook/vue3-vite',
